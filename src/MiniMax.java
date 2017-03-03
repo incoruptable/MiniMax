dd9a02a7b90a6577a20d09fce66d44fa7343c076
incoruptable <zachbrandt51@yahoo.com>
2017-03-03 14:21:23 -0600

    Almost there. COMPUTERS WILL WIN

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -1,17 +1,118 @@
+import java.util.Scanner;
+
 /**
  * Created by jisazb1 on 2/27/2017.
  */
 public class MiniMax {
 
+    static Node bestOption;
     Node root;
 
     public MiniMax() {
         initializeTree();
+        boolean stop = false;
+        Scanner scanner = new Scanner(System.in);
+        Node prevState = root;
+        Node currentState;
+        System.out.println("Hello HUMAN! Welcome to tic-tac toe! Good luck! ;)\n");
+        printState(root.getNodeState());
+
+//        //TESTING
+//        currentState = prevState.getChildForPosition(0);
+//        prevState = currentState;
+//        currentState = prevState.getChildForPosition(3);
+//        prevState = currentState;
+//        currentState = prevState.getChildForPosition(6);
+//        prevState = currentState;
+//        currentState = prevState.getChildForPosition(4);
+//        printState(currentState.getNodeState());
+//        prevState = currentState;
+        while (!stop) {
+
+            System.out.println("Enter which space to place an X:");
+            int position = scanner.nextInt();
+            if (position <= 0 || position >= 10) {
+                System.out.print("This position doesn't exist. What are you doing? ");
+                continue;
+            }
+            currentState = prevState.getChildForPosition(position - 1);
+            if (currentState == null) {
+                System.out.print("This position is already filled. Please try again. ");
+                continue;
+            }
+            if (currentState.getResultOfGame() != 0) {
+                if (currentState.getResultOfGame() == -1) {
+                    System.out.print("YOU WIN THIS TIME HUMAN! :(");
+                } else {
+                    System.out.print("WE ARE EVENLY MATCHED I SEE!");
+                }
+                return;
+            }
+            printState(currentState.getNodeState());
+            System.out.print("\n");
+            prevState = currentState;
+            currentState = findBestMove(prevState);
+            printState(currentState.getNodeState());
+
+            if (currentState.getResultOfGame() != 0) {
+                if (currentState.getResultOfGame() == 1) {
+                    System.out.print("I KNEW YOU WERE NO MATCH FOR ME!");
+                } else {
+                    System.out.print("WE ARE EVENLY MATCHED I SEE!");
+                }
+            }
+            System.out.print("\n");
+            prevState = currentState;
+
+        }
+
     }
 
     public static void main(String args[]) {
         MiniMax game = new MiniMax();
     }
+
+    public Node findBestMove(Node currentState) {
+        int bestValue = -2000000;
+        Node bestMove = currentState;
+        for (Node child : currentState.children) {
+
+            int currentValue = miniMax(child, 0, 1);
+            if (currentValue > bestValue) {
+                bestMove = child;
+                bestValue = currentValue;
+            }
+
+        }
+        return bestMove;
+    }
+
+    public int miniMax(Node node, int depth, int player) {
+        if (node.getResultOfGame() != 0) {
+            return node.getResultOfGame();
+        }
+        int bestValue;
+        if (player == 1) {
+            bestValue = -2000000;
+            for (Node child : node.children) {
+                int childValue = miniMax(child, depth + 1, -1);
+                if (childValue > bestValue) {
+                    bestValue = childValue - depth;
+                }
+            }
+        } else {
+            bestValue = 2000000;
+            for (Node child : node.children) {
+                int childValue = miniMax(child, depth + 1, 1);
+                if (childValue < bestValue) {
+                    bestValue = childValue - depth;
+                }
+            }
+        }
+
+        return bestValue;
+    }
+
 
     public void printState(int[] state) {
         for (int i = 0; i < 3; i++) {
@@ -34,20 +135,20 @@ public class MiniMax {
     }
 
     public void initializeTree() {
-        root = new Node(null);
-        root.setNodeState(new int[9]);
-        generateChildren(root, 0, -1);
+        root = generateChildren(null, 0, -1);
     }
 
     public Node generateChildren(Node parent, int player, int position) {
         Node currentNode = new Node(parent);
         if (player != 0) {
-            currentNode.setNodeState(parent.getNodeState(), player, position);
+            currentNode.setNodeState(player, position);
         }
-
-        for (int i = 0; i < 9; i++) {
-            if (currentNode.isValidMove(i)) {
-                currentNode.children.add(generateChildren(currentNode, changePlayer(player), position));
+        boolean gameOver = currentNode.checkIfGameOver();
+        if (!gameOver) {
+            for (int i = 0; i < 9; i++) {
+                if (currentNode.getNodeState()[i] == 0) {
+                    currentNode.children.add(generateChildren(currentNode, changePlayer(player), i));
+                }
             }
         }
         return currentNode;
@@ -58,7 +159,7 @@ public class MiniMax {
             return -1;
         } else if (player == -1) {
             return 1;
-        } else if (player == 1) {
+        } else {
             return -1;
         }
     }
--- src/Node.java
+++ src/Node.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Node {
     public List<Node> children;
     private Node parent;
-    private int resultOfGame;
+    private int resultOfGame; //2 means that computer has won, 1 means that the game was a tie,0 means that the game was a tie, -1 means that human has won,
     private int[] nodeState; //1 means that an O has been placed(Computer), 0 means that nothing is there, -1 means that an X has been placed(Human)
 
     public Node(Node parent) {
@@ -13,8 +13,8 @@ public class Node {
         children = new ArrayList<>();
     }
 
-    public void setNodeState(int[] nodeState, int player, int position) {
-        this.nodeState = nodeState;
+    public void setNodeState(int player, int position) {
+        this.nodeState = parent.getNodeState().clone();
         if (player == 1) {
             nodeState[position] = 1;
         } else if (player == -1) {
@@ -25,11 +25,143 @@ public class Node {
         }
     }
 
+    public Node getChildForPosition(int position) {
+        if (nodeState[position] != 0) {
+            return null;
+        }
+
+        for (Node child : children) {
+            if (child.getNodeState()[position] != 0) {
+                return child;
+            }
+        }
+        return null;
+    }
+
+    public boolean checkIfGameOver() {
+        //Check each possible situation where the game would end
+        //Check for Wins across
+        for (int i = 0; i < 3; i++) {
+            if (nodeState[i * 3] == 1) {
+                FOUND:
+                {
+                    for (int j = 1; j < 3; j++) {
+                        if (nodeState[i * 3 + j] != 1) {
+                            break FOUND;
+                        }
+                    }
+                    resultOfGame = 2;
+                    return true;
+                }
+            } else if (nodeState[i * 3] == -1) {
+                FOUND:
+                {
+                    for (int j = 1; j < 3; j++) {
+                        if (nodeState[i * 3 + j] != -1) {
+                            break FOUND;
+                        }
+                    }
+                    resultOfGame = -1;
+                    return true;
+                }
+            }
+        }
+
+
+        //Check for wins down
+        for (int i = 0; i < 3; i++) {
+            if (nodeState[i] == 1) {
+                FOUND:
+                {
+                    for (int j = 1; j < 3; j++) {
+                        if (nodeState[i + j * 3] != 1)
+                            break FOUND;
+                    }
+                    resultOfGame = 2;
+                    return true;
+                }
+            } else if (nodeState[i] == -1) {
+                FOUND:
+                {
+                    for (int j = 1; j < 3; j++) {
+                        if (nodeState[i + j * 3] != -1)
+                            break FOUND;
+                    }
+                    resultOfGame = -1;
+                    return true;
+                }
+            }
+        }
+
+        //check for wins diagonally
+        //check from left to right
+        if (nodeState[0] == 1) {
+            FOUND:
+            {
+                for (int i = 1; i < 3; i++) {
+                    if (nodeState[i * 4] != 1) {
+                        break FOUND;
+                    }
+                }
+                resultOfGame = 2;
+                return true;
+            }
+        } else if (nodeState[0] == -1) {
+            FOUND:
+            {
+                for (int i = 1; i < 3; i++) {
+                    if (nodeState[i * 4] != -1) {
+                        break FOUND;
+                    }
+                }
+                resultOfGame = -1;
+                return true;
+            }
+        }
+
+        //check from right to left
+        if (nodeState[2] == 1) {
+            FOUND:
+            {
+                for (int i = 1; i < 3; i++) {
+                    if (nodeState[i * 2 + 2] != 1) {
+                        break FOUND;
+                    }
+                }
+                resultOfGame = 2;
+                return true;
+            }
+        } else if (nodeState[2] == -1) {
+            FOUND:
+            {
+                for (int i = 1; i < 3; i++) {
+                    if (nodeState[i * 2 + 2] != -1) {
+                        break FOUND;
+                    }
+                }
+                resultOfGame = -1;
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (nodeState[i] == 0) {
+                return false;
+            }
+        }
+        resultOfGame = 1;
+        return true;
+    }
+
     public int[] getNodeState() {
         return nodeState;
     }
 
     public void setNodeState(int[] nodeState) {
         this.nodeState = nodeState;
+    }
+
+    public int getResultOfGame() {
+        return this.resultOfGame;
     }
 }
